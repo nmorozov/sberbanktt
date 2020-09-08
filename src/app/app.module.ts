c552f103f3b7b718d1602d7a3009f30519e1f925
Nikita Morozov <nikmorze@gmail.com>
2020-09-08 13:47:35 +0400

    New imports were added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,66 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskModule } from 'ngx-mask';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { UserInfoComponent } from './components/checkout/components/user-info/user-info.component';
+import { DeliveryComponent } from './components/checkout/components/delivery/delivery.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CheckoutComponent,
+    UserInfoComponent,
+    DeliveryComponent,
+    DialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgxMaskModule.forRoot(),
+    MatButtonModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    HttpClientModule,
+    MatDialogModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: {
+        parse: {
+          dateInput: ['l', 'LL'],
+        },
+        display: {
+          dateInput: 'L',
+          monthYearLabel: 'MMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+        },
+      },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
